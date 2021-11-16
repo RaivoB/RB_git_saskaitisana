8df490ff81257f6903d074b58c3f76d68687d34e
Raivo Bukovskis <rb2814@gymn.local>
2021-11-16 09:23:06 +0200

    Parveidojam funkciju ar 3 skaitliem meginajums 2

--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 
 using namespace std;
 
-int SUS( int a, int b);
+int SUS( int a, int b, int c);
 
 int main()
 {
     int x;
     int y;
+    int u;
 
     cout << "Ievadiet 1.ciparu: ";
     cin >> x;
     cout <<"Ievadiet 2.ciparu: ";
     cin >> y;
+    cout <<"Ievadiet 3.ciparu: ";
+    cin >> u;
 
-    cout <<"Jusu ievadito skaitlu summa ir "<< SUS(x,y) << endl;
+    cout <<"Jusu ievadito skaitlu summa ir "<< SUS(x,y,u) << endl;
 
     return 0;
 }
 
-int SUS(int a, int b)
+int SUS(int a, int b, int c)
 {
     int r;
-    r = a + b;
+    r = a + b + c;
 
     return r;
 }
